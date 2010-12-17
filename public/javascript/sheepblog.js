850c598ddcd5e8f14cc9a697eb3f0071e58c127a
Larger One <inhortte@gmail.com>
2010-12-16 22:05:31 -0600

    sidebar finished! now for the ajax stuff!

---
+++ public/javascript/sheepblog.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $("li[id^='ent'] > a").click(function() {
+	$("#sidebar > ul > li > ul > li").each(function() {
+	    $(this).removeClass('block');
+	    $(this).addClass('none');
+	    $(this).find("ul > li").each(function() {
+		$(this).removeClass('block');
+		$(this).addClass('none');
+	    });
+	});
+	var li_id = $(this).parent().attr("id").substr(3);
+
+	// A year has been clicked.
+	if(li_id.length == 4) {
+	    $(this).siblings("ul").children("li").each(function() {
+		$(this).removeClass('none');
+		$(this).addClass('block');
+	    });
+	} else {
+	    // A month has been clicked.
+	    $(this).parent().removeClass('none');
+	    $(this).parent().addClass('block');
+	    $(this).parent().siblings("li").each(function() {
+		$(this).removeClass('none');
+		$(this).addClass('block');
+	    });
+	    $(this).siblings("ul").children("li").each(function() {
+		$(this).removeClass('none');
+		$(this).addClass('block');
+	    });
+	}
+    });
+});
