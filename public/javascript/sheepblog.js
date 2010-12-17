05313f6249b5a11d328d5923fd3635d14c13890b
Larger One <inhortte@gmail.com>
2010-12-16 22:44:45 -0600

    cleaned up the javascript a bit

--- public/javascript/sheepblog.js
+++ public/javascript/sheepblog.js
@@ -1,33 +1,30 @@
+function li_to_block(li) {
+    $(li).removeClass('none');
+    $(li).addClass('block');
+}
+
 $(document).ready(function() {
     $("li[id^='ent'] > a").click(function() {
 	var li_id = $(this).parent().attr("id").substr(3);
 
-	$("#sidebar > ul > li > ul > li").each(function() {
+	$("#sidebar > ul > li > ul").find("li").each(function() {
 	    $(this).removeClass('block');
 	    $(this).addClass('none');
-	    $(this).find("ul > li").each(function() {
-		$(this).removeClass('block');
-		$(this).addClass('none');
-	    });
 	});
 
 	// A year has been clicked.
 	if(li_id.length == 4) {
 	    $(this).siblings("ul").children("li").each(function() {
-		$(this).removeClass('none');
-		$(this).addClass('block');
+		li_to_block(this);
 	    });
 	} else {
 	    // A month has been clicked.
-	    $(this).parent().removeClass('none');
-	    $(this).parent().addClass('block');
+	    li_to_block($(this).parent().get(0));
 	    $(this).parent().siblings("li").each(function() {
-		$(this).removeClass('none');
-		$(this).addClass('block');
+		li_to_block(this);
 	    });
 	    $(this).siblings("ul").children("li").each(function() {
-		$(this).removeClass('none');
-		$(this).addClass('block');
+		li_to_block(this);
 	    });
 	}
     });
